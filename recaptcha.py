01312cc943d4074bb3ffdb72316bc2f630b9f767
Radon Rosborough <radon@intuitiveexplanations.com>
2022-10-24 20:27:44 -0700

    I am legit not sure how this ever worked

--- recaptcha.py
+++ recaptcha.py
@@ -3,6 +3,7 @@ import webbrowser
 
 import flask
 
+
 def run_app(q):
     app = flask.Flask(__name__)
 
@@ -25,9 +26,7 @@ def run_app(q):
 def get_token(site_key):
     q = multiprocessing.Queue(maxsize=1)
 
-    server = multiprocessing.Process(
-        target=run_app(q), daemon=True
-    )
+    server = multiprocessing.Process(target=lambda: run_app(q), daemon=True)
     server.start()
     webbrowser.open(f"http://localhost:8888#{site_key}")
 
